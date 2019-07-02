4a59b40c35155751bd1400a9410af332d51b0c43
stefian <v_ianta@yahoo.com>
2019-07-02 15:32:21 -0400

    added validation for age

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -5,16 +5,21 @@
         <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
-            type="email"
-            id="email"
-            @blur="$v.email.$touch()"
-            v-model="email" />
+                  type="email"
+                  id="email"
+                  @blur="$v.email.$touch()"
+                  v-model="email" />
           <p v-if="!$v.email.email">Please provide a valid email address.</p>
           <p v-if="!$v.email.required">This field must not be empty.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
-          <input type="number" id="age" v-model.number="age" />
+          <input
+                  type="number"
+                  id="age"
+                  @blur="$v.age.$touch()"
+                  v-model.number="age" />
+          <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} y.o. 18 for ai patenting.</p>
         </div>
         <div class="input">
           <label for="password">Password</label>
@@ -61,7 +66,7 @@
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators'
+import { required, email, numeric, minValue } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
@@ -78,6 +83,11 @@ export default {
     email: {
       required,
       email
+    },
+    age: {
+      required,
+      numeric,
+      minVal: minValue(10)
     }
   },
   methods: {
