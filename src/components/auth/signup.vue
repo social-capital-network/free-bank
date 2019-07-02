a4ec8189156dad49aa9ebf22731dd0486d09596e
stefian <v_ianta@yahoo.com>
2019-07-02 16:17:03 -0400

    added confirm pass validator

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -29,9 +29,13 @@
                   @blur="$v.password.$touch()"
                   v-model="password" />
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
-          <input type="password" id="confirm-password" v-model="confirmPassword" />
+          <input
+                  type="password"
+                  id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
+                  v-model="confirmPassword" />
         </div>
         <div class="input">
           <label for="country">Country</label>
@@ -70,7 +74,7 @@
 </template>
 
 <script>
-import { required, email, numeric, minValue, minLength } from 'vuelidate/lib/validators'
+import { required, email, numeric, minValue, minLength, sameAs } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
@@ -96,6 +100,12 @@ export default {
     password: {
       required,
       minLen: minLength(6)
+    },
+    confirmPassword: {
+      // sameAs: sameAs('password')
+      sameAs: sameAs(vm => {
+        return vm.password
+      })
     }
   },
   methods: {
