5e9fb52276a39c3027249ea91bcbb5c6b1552fb9
stefian <v_ianta@yahoo.com>
2019-07-02 13:22:18 -0400

    added Auto Login and mgmt of user items in local storage

--- src/store.js
+++ src/store.js
@@ -46,6 +46,11 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           })
+          const now = new Date()
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+          localStorage.setItem('token', res.data.idToken)
+          localStorage.setItem('userId', res.data.localId)
+          localStorage.setItem('expirationDate', expirationDate)
           dispatch('storeUser', authData)
           dispatch('setLogoutTimer', res.data.expiresIn)
         })
@@ -60,6 +65,11 @@ export default new Vuex.Store({
         })
         .then(res => {
           console.log(res)
+          const now = new Date()
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+          localStorage.setItem('token', res.data.idToken)
+          localStorage.setItem('userId', res.data.localId)
+          localStorage.setItem('expirationDate', expirationDate)
           commit('authUser', {
             token: res.data.idToken,
             userId: res.data.localId
@@ -68,8 +78,27 @@ export default new Vuex.Store({
         })
         .catch(error => console.log(error))
     },
+    tryAutoLogin ({ commit }) {
+      const token = localStorage.getItem('token')
+      if (!token) {
+        return
+      }
+      const expirationDate = localStorage.getItem('expirationDate')
+      const now = new Date()
+      if (now >= expirationDate) {
+        return
+      }
+      const userId = localStorage.getItem('userId')
+      commit('authUser', {
+        token: token,
+        userId: userId
+      })
+    },
     logout ({ commit }) {
       commit('clearAuthData')
+      localStorage.removeItem('expirationDate')
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
       router.replace('/signin')
     },
     storeUser ({ commit, state }, userData) {
