05144a403537516cc61c0e5e45373cfa1418908b
stefian <v_ianta@yahoo.com>
2019-07-05 13:43:00 -0400

    styling the cells/neurons

--- src/components/cell/cell.vue
+++ src/components/cell/cell.vue
@@ -1,14 +1,53 @@
 <template>
   <div class="cell">
+    <div class="outer">
+      <div class="inner">
+        <i class="top left"></i>
+        <i class="top right"></i>
+        <div class="content"></div>
+        <i class="bottom right"></i>
+        <i class="bottom left"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
   .cell {
-    width: 10px;
-    height: 10px;
     background-color: #ccc;
-    border: 1px solid #ccc;
-    margin: 1px;
+    border: 0px solid #fff;
+    margin: 0px;
   }
+
+  .outer {
+  overflow: hidden;
+}
+.inner {
+  border: 0px solid #888;
+}
+.inner i {
+  width: 12px;
+  height: 12px;
+  border: 0px solid #888;
+  border-radius: 50%;
+  background-color: #fff;
+}
+.inner .top {
+  margin-top: -6px;
+}
+.inner .bottom {
+  margin-top: -6px;
+  margin-bottom: -6px;
+}
+.inner .left {
+  float: left;
+  margin-left: -6px;
+}
+.inner .right {
+  float: right;
+  margin-right: -6px;
+}
+.content {
+  min-height: 14px;
+}
 </style>
